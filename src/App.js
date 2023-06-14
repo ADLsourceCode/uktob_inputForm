450ccc3a9c4f3d36d693979b98bb0b5a507c8e01
Akshay <alamba093@gmail.com>
2023-06-14 11:06:42 +0400

    form with input and button  added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [inputString, setInputString] = useState('');
+  const [inputNumber, setInputNumber] = useState('');
+  const [outputText, setOutputText] = useState('');
+  const [error, setError] = useState('');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="stringInput">Enter a string: </label>
+          <input
+            id="stringInput"
+            type="text"
+            value={inputString}
+            onChange={(e) => setInputString(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="numberInput">Enter a number: </label>
+          <input
+            id="numberInput"
+            type="text"
+            value={inputNumber}
+            onChange={(e) => setInputNumber(e.target.value)}
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+
     </div>
   );
 }
